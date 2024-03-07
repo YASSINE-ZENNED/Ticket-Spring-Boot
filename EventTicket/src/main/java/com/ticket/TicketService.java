26b278f0d1128a8b43335d435f97ef14fef11eaa
abir ben khaled <benkhaledabir27@gmail.com>
2024-03-07 15:16:25 +0100

    crud EventTicket

--- EventTicket/src/main/java/com/ticket/TicketService.java
+++ EventTicket/src/main/java/com/ticket/TicketService.java
@@ -3,6 +3,9 @@ package com.ticket;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @AllArgsConstructor
 
 @Service
@@ -10,6 +13,11 @@ public class TicketService {
 
     private final TicketRepository ticketRepository;
 
+
+        public List<Ticket> getTicket(){
+
+            return ticketRepository.findAll();
+        }
         public void createTicket(TicketCreationRequest ticketCreationRequest) {
             Ticket ticket = Ticket.builder()
                     .ticketType(ticketCreationRequest.ticketType())
@@ -22,5 +30,25 @@ public class TicketService {
             ticketRepository.saveAndFlush(ticket);
             System.out.println("Creating a ticket");
         }
+    public void updateTicket(Long ticketId, TicketCreationRequest ticketCreationRequest) {
+        Ticket existingTicket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new TicketNotFoundException("Ticket not found with ID: " + ticketId));
+//        existingTicket.setId(ticketCreationRequest.id());
+        existingTicket.setTicketType(ticketCreationRequest.ticketType());
+        existingTicket.setEventId(ticketCreationRequest.eventId());
+        existingTicket.setTicketPrice(ticketCreationRequest.ticketPrice());
+        existingTicket.setTicketTime(ticketCreationRequest.ticketTime());
+
+
+        ticketRepository.saveAndFlush(existingTicket);
+        System.out.println("Updating a ticket with ID: " + ticketId);
+    }
+    public void deleteTicket(Long ticketId) {
+        Ticket existingTicket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new TicketNotFoundException("Ticket not found with ID: " + ticketId));
+
+        ticketRepository.delete(existingTicket);
+        System.out.println("Deleting a ticket with ID: " + ticketId);
+    }
 
 }
