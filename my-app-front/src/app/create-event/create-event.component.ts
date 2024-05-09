de0790888ced8cdddfd6263de6a07ef04e4e34fc
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2024-05-09 16:07:03 +0100

    cretae event semi-works

--- my-app-front/src/app/create-event/create-event.component.ts
+++ my-app-front/src/app/create-event/create-event.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {SharedService} from "../shared.service";
+ interface Event {
+  eventName: string;
+  date: string | null;
+  location: string;
+  numberOfSeats: number;
+}
 
 @Component({
   selector: 'app-create-event',
@@ -6,5 +15,43 @@ import { Component } from '@angular/core';
   styleUrl: './create-event.component.css'
 })
 export class CreateEventComponent {
+  constructor(private http: HttpClient, private router: Router,public sharedService: SharedService) {}
+  errorMessage = '';
+
+  event: Event = {
+    eventName: "Event3",
+    date: new Date().toISOString().split('T')[0],
+    location: "tekup",
+    numberOfSeats: 90
+  };
+  onSubmit() {
+    console.log('Getting __________________________ successful!');
+
+    let   token = this.sharedService.Token ;
+
+     const headers = new HttpHeaders({
+       'Authorization': `Bearer ${token}`
+     });
+
+    this.http.post<any>( // Specify the expected response type
+      'http://localhost:8082/events/CreateEvent',
+      this.event,
+      { headers }
+          ).subscribe(
+      response => {
+
+        console.log('adding successful!', response);
+
+        this.errorMessage = ''; // Clear any previous error message
+
+      },
+      error => {
+        console.error('adding events  failed:', error);
+        this.errorMessage = 'adding events. Please check your credentials.'; // Set an error message
+      }
+    );
+
+  }
+
 
 }
