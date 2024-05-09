f2c7d24e66d3b2d14094aee4858bf4a2d14452dd
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2024-05-09 16:34:21 +0100

    trty

--- my-app-front/src/app/add-ticket/add-ticket.component.ts
+++ my-app-front/src/app/add-ticket/add-ticket.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {SharedService} from "../shared.service";
+
+interface Ticket {
+  eventId: number
+  numberOfSeats: number;
+}
 
 @Component({
   selector: 'app-add-ticket',
@@ -6,5 +14,42 @@ import { Component } from '@angular/core';
   styleUrl: './add-ticket.component.css'
 })
 export class AddTicketComponent {
+
+  constructor(private http: HttpClient, private router: Router,public sharedService: SharedService) {}
+  errorMessage = '';
+
+  ticket: Ticket = {
+    eventId: 1,
+    numberOfSeats: 90
+  };
+
+  onSubmit() {
+    console.log('Getting __________________________ successful!');
+
+    let   token = this.sharedService.Token ;
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    this.http.post<any>( // Specify the expected response type
+      'http://localhost:8083/ticket',
+      this.ticket,
+      { headers }
+    ).subscribe(
+      response => {
+
+        console.log('adding successful!', response);
+
+        this.errorMessage = ''; // Clear any previous error message
+
+      },
+      error => {
+        console.error('adding events  failed:', error);
+        this.errorMessage = 'adding events. Please check your credentials.'; // Set an error message
+      }
+    );
+
+  }
 
 }
